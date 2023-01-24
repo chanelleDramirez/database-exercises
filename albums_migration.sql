966a1ef8da86247e03d7ec945a16ec7fa925511c
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-24 09:10:15 -0600

    tables exercise

---
+++ albums_migration.sql
@@ -0,0 +1,10 @@
+USE codeup_test_db;
+CREATE TABLE quotes (
+                        id ,auto-incrementing unsigned integer primary key
+                        artist'string for storing the recording artist name',
+                        name  'string for storing a record name',
+                        release_date integer representing year record was released,
+                        sales  floating point value for number of records sold (in millions)
+                        genre 'string for storing the record''s genre(s)'
+                        PRIMARY KEY (id)
+);
