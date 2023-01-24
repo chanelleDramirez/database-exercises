ab4ed51e2be004703916393fe856a39db4ae41a0
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-24 09:15:59 -0600

    database exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,10 @@
 USE codeup_test_db;
 CREATE TABLE quotes (
                         id ,auto-incrementing unsigned integer primary key
-                        artist'string for storing the recording artist name',
-                        name  'string for storing a record name',
-                        release_date integer representing year record was released,
-                        sales  floating point value for number of records sold (in millions)
+                        artist VARCHAR(50),
+                        name VARCHAR(100),
+                        release_date INT,
+                        sales  float,
                         genre 'string for storing the record''s genre(s)'
                         PRIMARY KEY (id)
 );
