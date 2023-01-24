9b815757ee5387c872f3794accbbdac3290c82e5
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-24 14:40:24 -0600

    Basic statements exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,10 +1,11 @@
 USE codeup_test_db;
+    DROP TABLE IF EXISTS album;
 CREATE TABLE quotes (
                         id ,auto-incrementing unsigned integer primary key
                         artist VARCHAR(50),
                         name VARCHAR(100),
                         release_date INT,
                         sales  float,
-                        genre 'string for storing the record''s genre(s)'
+                        genre VARCHAR(50),
                         PRIMARY KEY (id)
 );
---
+++ albums_seeder.sql
@@ -0,0 +1,29 @@
+USE codeup_test_db;
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Michael Jackson','Thriller','1982','51.2','Pop, post-disco, funk, rock');
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES  ('AC/DC', 'Back in Black','1980','30.1','Hard rock');
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Eagles','Their Greatest Hits (1971-1975)','1976','41.2','Country rock, soft rock, folk rock');
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Eagles','Hotel California','1976','31.8','Soft rock');
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Shania Twain','Come On Over','1997','30.4','Country, pop');
+
+INSERT INTO albums (artist,name,release_date,sales,genre)
+VALUES ('Led Zeppelin','Led Zeppelin IV','1971','30.4','Hard rock, heavy metal, folk rock');
+
+
+
+--     INSERT INTO albums (artist,name,release_date,sales,genre)
+-- VALUES ('Michael Jackson','Thriller','1982','51.2','Pop, post-disco, funk, rock');
+--         ('AC/DC', 'Back in Black','1980','30.1','Hard rock');
+--         ('Eagles','Their Greatest Hits (1971-1975)','1976','41.2','Country rock, soft rock, folk rock');
+--         ('Eagles','Hotel California','1976','31.8','Soft rock');
+--         ('Shania Twain','Come On Over','1997','30.4','Country, pop');
+--         ('Led Zeppelin','Led Zeppelin IV','1971','30.4','Hard rock, heavy metal, folk rock');
