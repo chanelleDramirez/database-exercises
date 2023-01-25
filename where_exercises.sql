9cd4e7a4b460320e9941698e2c507301da6790bc
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-25 16:48:32 -0600

    Where exercise

--- where_exercises.sql
+++ where_exercises.sql
@@ -1,3 +1,9 @@
 USE employees;
 
-SELECT first_name FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+#  // Part One
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+# Part Two
+
