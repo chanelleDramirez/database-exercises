a0c8ffcb63dc98e43aa05d4b339483e3cdac7d3c
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-25 16:16:26 -0600

    Where exercise

--- albums_migration.sql
+++ albums_migration.sql
@@ -1,11 +1,11 @@
 USE codeup_test_db;
-    DROP TABLE IF EXISTS album;
-CREATE TABLE quotes (
-                        id ,auto-incrementing unsigned integer primary key
+    DROP TABLE IF EXISTS albums;
+CREATE TABLE albums (
+                        id INT unsigned AUTO_INCREMENT,
                         artist VARCHAR(50),
                         name VARCHAR(100),
                         release_date INT,
                         sales  float,
-                        genre(s) VARCHAR(50),
+                        genre VARCHAR(50),
                         PRIMARY KEY (id)
 );
--- albums_seeder.sql
+++ albums_seeder.sql
@@ -20,15 +20,15 @@ USE codeup_test_db;
 
 
 
-    INSERT INTO albums (artist,name,release_date,sales,genre)
-VALUES ('Michael Jackson','Thriller','1982','51.2','Pop, post-disco, funk, rock');
-        ('AC/DC', 'Back in Black','1980','30.1','Hard rock');
-        ('Eagles','Their Greatest Hits (1971-1975)','1976','41.2','Country rock, soft rock, folk rock');
-        ('Eagles','Hotel California','1976','31.8','Soft rock');
-        ('Shania Twain','Come On Over','1997','30.4','Country, pop');
-        ('Led Zeppelin','Led Zeppelin IV','1971','30.4','Hard rock, heavy metal, folk rock');
-        ('Pink Floyd', 'The Dark Side of the Moon','1973','24.8','Progressive rock');
-        ('Pink Floyd', 'The Wall','1979','18.9','Progressive rock');
-        ('Pink Floyd', 'Wish You Were Here','1975','9.2','Progressive rock, art rock, experimental rock');
-        ('The Beatles', 'Sgt. Pepper''s Lonely Hearts Club Band', '1967', '17.0','Rock');
+    INSERT INTO albums (artist, name, release_date, sales, genre)
+VALUES ('Michael Jackson','Thriller','1982','51.2','Pop, post-disco, funk, rock'),
+        ('AC/DC', 'Back in Black','1980','30.1','Hard rock'),
+        ('Eagles','Their Greatest Hits (1971-1975)','1976','41.2','Country rock, soft rock, folk rock'),
+        ('Eagles','Hotel California','1976','31.8','Soft rock'),
+        ('Shania Twain','Come On Over','1997','30.4','Country, pop'),
+        ('Led Zeppelin','Led Zeppelin IV','1971','30.4','Hard rock, heavy metal, folk rock'),
+        ('Pink Floyd', 'The Dark Side of the Moon','1973','24.8','Progressive rock'),
+        ('Pink Floyd', 'The Wall','1979','18.9','Progressive rock'),
+        ('Pink Floyd', 'Wish You Were Here','1975','9.2','Progressive rock, art rock, experimental rock'),
+        ('The Beatles', 'Sgt. Peppers Lonely Hearts Club Band', '1967', '17.0','Rock'),
         ('Nirvana', 'Nevermind', '1992','17.8','Grunge, alternative rock');
