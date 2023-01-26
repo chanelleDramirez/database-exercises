bb2e318da08e5a502d6c45c9cbfaf5370b36c3d7
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-26 13:38:48 -0600

    Order By exercise completed

--- src/order_by_exercises.sql
+++ src/order_by_exercises.sql
@@ -1,14 +1,9 @@
 USE employees;
 
 #  // Part One
-SELECT first_name,last_name FROM employees ORDER BY last_name IN ('Irena','Vidya','Maya');
-SELECT * FROM employees WHERE last_name LIKE 'E%';
-SELECT * FROM employees WHERE last_name LIKE '%q%';
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya') ORDER BY first_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY first_name, last_name;
+SELECT * FROM employees WHERE first_name IN ('Irena', 'Vidya', 'Maya') ORDER BY last_name DESC, first_name DESC;
 
-# Part Two
-
-SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
-SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR  last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name LIKE 'E%' AND  last_name LIKE '%E';
-SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%';
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no;
+SELECT * FROM employees WHERE last_name LIKE 'E%' ORDER BY emp_no DESC;
