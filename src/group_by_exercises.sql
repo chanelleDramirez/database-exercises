cc3c59f1eb3377e1eebdf41504817e55a95e3aa1
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-26 16:36:39 -0600

    Group By exercise

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -5,3 +5,5 @@ SELECT DISTINCT title FROM titles;
 SELECT last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%' GROUP BY last_name ;
 
 SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
+
+SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%' GROUP BY last_name;
