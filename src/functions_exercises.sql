4ac468bf8bb3325762f9944c2868d3b41250c4b9
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-26 14:17:22 -0600

    Functions exercise completed

--- src/functions_exercises.sql
+++ src/functions_exercises.sql
@@ -11,3 +11,9 @@ USE employees;
 SELECT CONCAT(first_name, ' ',last_name) FROM employees WHERE first_name LIKE 'E%' AND last_name LIKE 'E%' AND first_name LIKE '%E' AND last_name LIKE '%E';
 
 SELECT * FROM employees WHERE month(birth_date) = 12 AND day(birth_date) = 25;
+
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25;
+
+SELECT * FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
+
+SELECT first_name ,last_name, DATEDIFF(NOW(),hire_date)  FROM employees WHERE YEAR(hire_date) BETWEEN 1990 AND 1999 AND MONTH(birth_date) = 12 AND DAYOFMONTH(birth_date) = 25 ORDER BY birth_date, hire_date DESC;
