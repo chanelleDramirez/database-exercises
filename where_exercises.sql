363817ba2e2f7ee9c1c186db9f3d3ffd9baff9aa
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-26 10:11:38 -0600

    WHERE exercise completed

--- where_exercises.sql
+++ where_exercises.sql
@@ -7,3 +7,8 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 # Part Two
 
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR  last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND  last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%';
