9e1cef00a8883375c376f081d1ced057a2b83dc1
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-26 10:36:39 -0600

    LIMIT exercise

---
+++ src/order_by_exercises.sql
@@ -0,0 +1,14 @@
+USE employees;
+
+#  // Part One
+SELECT first_name,last_name FROM employees ORDER BY last_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE last_name LIKE 'E%';
+SELECT * FROM employees WHERE last_name LIKE '%q%';
+
+# Part Two
+
+SELECT * FROM employees WHERE first_name IN ('Irena','Vidya','Maya');
+SELECT * FROM employees WHERE (first_name = 'Irena' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR  last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE 'E%' AND  last_name LIKE '%E';
+SELECT * FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%';
