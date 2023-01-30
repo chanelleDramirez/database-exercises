b5770886a0d55bc5c8803699387c28f70aef785b
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-29 23:57:06 -0600

    Aliases exercise

--- src/aliases_exercises.sql
+++ src/aliases_exercises.sql
@@ -2,4 +2,4 @@ USE employees;
 
 SELECT CONCAT(last_name, ' ', first_name) AS full_name FROM employees LIMIT 10;
 
-SELECT CONCAT(last_name, ' ', first_name) AS full_name FROM employees LIMIT 10;
+SELECT CONCAT(last_name, ' ', first_name, ' ',birth_date) AS full_name FROM employees  LIMIT 10;
