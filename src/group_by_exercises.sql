dfac00fb0218646233aa41386a30cea78e8bd828
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-01-29 23:22:30 -0600

    Group By exercise

--- src/group_by_exercises.sql
+++ src/group_by_exercises.sql
@@ -7,3 +7,13 @@ SELECT last_name FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%
 SELECT CONCAT(first_name, ' ', last_name) FROM employees WHERE last_name LIKE '%E' AND last_name LIKE 'E%';
 
 SELECT last_name FROM employees WHERE last_name LIKE '%q%' AND last_name  NOT LIKE '%qu%' GROUP BY last_name;
+
+SELECT COUNT(last_name) FROM employees WHERE last_name LIKE 'Chleq';
+
+SELECT COUNT(last_name) FROM employees WHERE last_name LIKE 'Irena';
+
+# SELECT COUNT(last_name) FROM employees WHERE last_name IN ('Irena','Vidya','Maya');
+
+
+# SELECT COUNT(last_name) FROM employees WHERE last_name LIKE 'Chleq'AND 'Lindqvist'AND'Qiwen';
+# SELECT COUNT(last_name) FROM employees WHERE last_name IN ('Chleq','Lindqvist','Qiwen');
