b2c4f6d43789748c758c8815ca7e883cf9033474
Chanelle Ramirez <chanelleramirez13@gmail.com>
2023-02-27 16:56:45 -0600

    Join exercise

---
+++ src/join_exercise.sql
@@ -0,0 +1,19 @@
+INSERT INTO users (name, email, role_id) VALUES
+                                             ('ashley','ashley@example.com', 2),
+                                             ('brion','brion@example.com', 2),
+                                             ('nell','nell@example.com', NULL);
+
+
+
+
+# JOIN
+SELECT columns
+FROM table_users as users
+         JOIN table_roles as roles ON users.id = roles.fk_id;
+
+
+
+# LEFT JOIN
+SELECT users.name AS user_name, roles.name AS role_name
+FROM users
+         LEFT JOIN roles ON users.role_id = roles.id;
---
+++ src/subqueries_exercises.sql
@@ -0,0 +1,3 @@
+USE employees;
+
+SELECT first_name, last_name, hire_date FROM employees WHERE emp_no IN ('101010');
